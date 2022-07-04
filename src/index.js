b3256aa667db094cea1b3d1d3ec71e9a47958d00
yoosion030 <rsy5487@naver.com>
2022-07-04 11:11:42 +0900

    Actions

--- src/index.js
+++ src/index.js
@@ -4,10 +4,23 @@ const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-const countModifier = (state = 0) => {
-  console.log(state);
-  return state;
+const countModifier = (count = 0, action) => {
+  if (action.type === "ADD") {
+    return count + 1;
+  } else if (action.type === "MINUS") {
+    return count - 1;
+  } else {
+    return count;
+  }
 };
 const countStore = legacy_createStore(countModifier);
+
+countStore.dispatch({ type: "ADD" }); //1
+countStore.dispatch({ type: "ADD" }); //2
+countStore.dispatch({ type: "ADD" }); //3
+countStore.dispatch({ type: "ADD" }); //4
+countStore.dispatch({ type: "ADD" }); //5
+countStore.dispatch({ type: "ADD" }); //6
+countStore.dispatch({ type: "MINUS" }); //5
 
 console.log(countStore.getState());
