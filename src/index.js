da44d2bf3488668c7896b0ec43e38069e2f984b2
yoosion030 <rsy5487@naver.com>
2022-07-04 12:04:14 +0900

    Recap Refactor

--- src/index.js
+++ src/index.js
@@ -5,15 +5,21 @@ const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
 number.innerText = 0;
+
+const ADD = "ADD";
+const MINUS = "MINUS";
+
 const countModifier = (count = 0, action) => {
-  if (action.type === "ADD") {
-    return count + 1;
-  } else if (action.type === "MINUS") {
-    return count - 1;
-  } else {
-    return count;
+  switch (action.type) {
+    case "ADD":
+      return count + 1;
+    case "MINUS":
+      return count - 1;
+    default:
+      return count;
   }
 };
+
 const countStore = legacy_createStore(countModifier);
 
 const onChange = () => {
@@ -22,5 +28,5 @@ const onChange = () => {
 
 countStore.subscribe(onChange);
 
-add.addEventListener("click", () => countStore.dispatch({ type: "ADD" }));
-minus.addEventListener("click", () => countStore.dispatch({ type: "MINUS" }));
+add.addEventListener("click", () => countStore.dispatch({ type: ADD }));
+minus.addEventListener("click", () => countStore.dispatch({ type: MINUS }));
