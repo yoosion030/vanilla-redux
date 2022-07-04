a2112744389cc173a665adb1b4e4869872a92289
yoosion030 <rsy5487@naver.com>
2022-07-04 23:51:53 +0900

    Detail Screen

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import { actionCreators } from "../store";
 
-function ToDo({ text, deleteToDo }) {
+function ToDo({ text, deleteToDo, id }) {
   return (
     <li>
-      {text} <button onClick={deleteToDo}>DEL</button>
+      <Link to={`/${id}`}>
+        {text} <button onClick={deleteToDo}>DEL</button>
+      </Link>
     </li>
   );
 }
