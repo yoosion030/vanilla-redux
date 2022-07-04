2787971a3be3f22cff2c7b13946c01819b8e8940
yoosion030 <rsy5487@naver.com>
2022-07-04 10:19:43 +0900

    Store and Reducer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,13 @@
+import { legacy_createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
 
-let count = 0;
-number.innerText = count;
-
-const updateText = () => {
-  number.innerText = count;
+const countModifier = (state = 0) => {
+  console.log(state);
+  return state;
 };
+const countStore = legacy_createStore(countModifier);
 
-const handleAdd = () => {
-  count += 1;
-  updateText();
-};
-
-const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+console.log(countStore.getState());
