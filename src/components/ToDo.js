5cf3b84f54df8130dc238fb1bfab53f0f5002e80
yoosion030 <rsy5487@naver.com>
2022-07-05 21:04:37 +0900

    createSlice

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-import { actionCreators } from "../store";
+import { actionCreators, remove } from "../store";
 
 function ToDo({ text, deleteToDo, id }) {
   return (
@@ -14,7 +14,7 @@ function ToDo({ text, deleteToDo, id }) {
 
 function mapDispatchToProps(dispatch, ownProps) {
   return {
-    deleteToDo: () => dispatch(actionCreators.deleteToDo(ownProps.id)),
+    deleteToDo: () => dispatch(remove(ownProps.id)),
   };
 }
 
--- src/store.js
+++ src/store.js
@@ -1,22 +1,17 @@
-import { legacy_createStore } from "redux";
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
-
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
+const toDos = createSlice({
+  name: "toDoReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
   },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
 });
 
-const store = configureStore({ reducer });
+export const { add, remove } = toDos.actions;
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
-
-export default store;
+export default configureStore({ reducer: toDos.reducer });
